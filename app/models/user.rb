6e930b438ca315b5cb403de20ecc0f1a59699869
Hoon Lee <yl.hoony@gmail.com>
2018-02-21 04:43:42 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ActiveRecord::Base
 
   devise :omniauthable, :omniauth_providers => [:facebook]
 
+  validates_length_of :password, minimum: 8
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end  
+  end
   
 end
